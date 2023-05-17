14e0c056a0f46a6354b8f70ee268d91599e58f26
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-17 11:32:29 +0530

    Implemented pagination to retrieve the repositories and their information, that overcomes the limitation of the maximum number of items returned by the Github API.

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 const container = document.getElementById("container");
 
-function requestUserRepos(username){
-    return Promise.resolve(fetch(`https://api.github.com/users/${username}/repos`));
+function requestUserRepos(username, page=1) {
+    return Promise.resolve(
+        fetch(`https://api.github.com/users/${username}/repos?page=${page}`)
+    );
 }
+
+async function getAllUserRepos(username) {
+    let page = 1;
+    let repos = [];
+
+    while (true) {
+        const response = await requestUserRepos(username, page);
+        const data = await response.json();
+
+        if (data.length === 0) {
+            break;
+        }
+        repos = repos.concat(data);
+        page++;
+    }
+    return repos;
+}
+
 username = prompt("Enter the username: ");
-requestUserRepos(username)
-    .then(response => response.json())
-    .then(data => {
+getAllUserRepos(username)
+    .then((data) => {
         for (let i in data) {
             const githubContainerChild = document.createElement("div");
             githubContainerChild.classList.add("container-child");
             
-            
             const githubContainerChildName = document.createElement("div");
             githubContainerChildName.classList.add("container-child-name");
             githubContainerChildName.textContent = data[i].name;
+            githubContainerChild.appendChild(githubContainerChildName);
 
             const githubContainerChildDescription = document.createElement("div");
             githubContainerChildDescription.classList.add("container-child-description");
             githubContainerChildDescription.textContent = data[i].description;
-
+            githubContainerChild.appendChild(githubContainerChildDescription);
             
             const githubContainerChildURL = document.createElement("div");
             githubContainerChildURL.classList.add("container-child-url");
             githubContainerChildURL.textContent = data[i].html_url;
-
-            githubContainerChild.appendChild(githubContainerChildName);
-            githubContainerChild.appendChild(githubContainerChildDescription);
             githubContainerChild.appendChild(githubContainerChildURL);
-
 
             container.appendChild(githubContainerChild);
         }
